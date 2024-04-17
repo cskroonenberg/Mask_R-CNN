fdf33547affba9b8cb9aca7afee2e04c6e9d061c
Nian Liu <nliu319@gatech.edu>
2024-04-16 23:27:31 -0700

    Updated RPN anchor_eval to sample 256 anchors (target 1:1 fg:bg) Updated RPN to only use anchors that fall within the image boundaries Updated RPN to send both fg and bg proposals to the classifier Updated Classifier to classify background boxes Fixed Classifier detach issues Added Classifier bbox regression to evaluate functions and screen out proposals that were classified as bg

--- networks/FRCClassifier.py
+++ networks/FRCClassifier.py
@@ -69,20 +69,28 @@ class FRCClassifier_fasteronly(nn.Module):
         # classification scores/loss
         class_scores = self.classifier(out)
         assigned_labels = torch.cat(assigned_labels, dim=0)
+        fg_mask = (assigned_labels != 0)
 
         # calculate cross entropy loss
         class_loss = nn.functional.cross_entropy(class_scores, assigned_labels)
 
         # box regression scores
         box_reg_scores = self.box_regressor(out)
+        box_reg_scores_fg = box_reg_scores[fg_mask, :]
+
 
         # determine truth deltas/masks for box regression
         truth_delta_masks = torch.zeros_like(box_reg_scores, dtype=torch.bool).to(self.device)
         for i, label in enumerate(assigned_labels):
             truth_delta_masks[i, (4 * label):(4 * label + 4)] = 1
 
+        truth_delta_masks_fg = truth_delta_masks[fg_mask, :]
+
+        truth_deltas = torch.cat(truth_deltas)
+        truth_deltas_fg = truth_deltas[fg_mask, :]
+
         # calculate box regression loss
-        box_reg_loss = self.box_reg_loss(box_reg_scores[truth_delta_masks].detach(), torch.cat(truth_deltas).flatten())
+        box_reg_loss = self.box_reg_loss(box_reg_scores_fg[truth_delta_masks_fg], truth_deltas_fg.flatten())
 
         return class_loss + box_reg_loss
 
@@ -100,7 +108,9 @@ class FRCClassifier_fasteronly(nn.Module):
         # classification scores
         scores = self.classifier(out)
 
-        return scores
+        box_deltas = self.box_regressor(out)
+
+        return scores, box_deltas
 
     @staticmethod
     def box_regression_loss(box_reg_scores, truth_deltas, truth_delta_masks, scale=1.0, sigma=1.0):
